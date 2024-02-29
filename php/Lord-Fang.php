3b06b2ee1bd4a18068084dc8bcb3dfed3fa88800
EmeraldieMoon <161104858+EmeraldieMoon@users.noreply.github.com>
2024-02-29 23:37:15 +0700

    Add files via upload

--- php/Lord-Fang.php
+++ php/Lord-Fang.php
@@ -9,7 +9,7 @@
 		<style>body {background:#15495F}</style>
 </head>
 <body>
-	<div class="bioPage Monster" style="background:url('https://i.ibb.co/MBwzZKS/f-Lord-Fang.jpg') no-repeat scroll 0 0 #fff">
+	<div class="bioPage Hero" style="background:url('https://i.ibb.co/MBwzZKS/f-Lord-Fang.jpg') no-repeat scroll 0 0 #fff">
 		<div class="textFrame switch">
 			<div class="bioText">
 				<h1>Lord Fang</h1>
@@ -21,7 +21,7 @@
 			<img src="https://i.ibb.co/68MYfdk/mini-H-Lord-Fang.png" alt="Lord Fang miniature" />
 		</div>
 		<div class="card switch">
-			<img src="https://i.ibb.co/bzfrngR/Lord-Fang.jpg" alt="Lord Fang profile card" width="380" height="531" />
+			<img src="https://i.ibb.co/bzfrngR/Lord-Fang.jpg" alt="Lord Fang profile card" width="531" height="380" />
 		</div>
 		<div class="switchButttons">
 			<span class="switchBio" title="Show biography"></span>
